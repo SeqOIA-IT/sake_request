dc462ba302366647c7c0723706b4b7a282f1990d
Pierre Marijon <pierre.marijon-ext@aphp.fr>
2024-11-13 17:38:33 +0100

    fix: test evaluate all path and number of thread

--- tests/test_obj.py
+++ tests/test_obj.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 # std import
+import os
 import pathlib
 
 # 3rd party import
@@ -17,9 +18,14 @@ def test_default_value() -> None:
     database = Sake(sake_path)
 
     assert database.sake_path == sake_path
+
+    assert os.environ["POLARS_MAX_THREADS"] == str(os.cpu_count())
+
     assert database.aggregations_path == sake_path / "aggregations"
     assert database.annotations_path == sake_path / "annotations"
+    assert database.partitions_path == sake_path / "{target}" / "genotypes" / "partitions"
     assert database.samples_path == sake_path / "samples" / "patients.parquet"
+    assert database.transmissions_path == sake_path / "{target}" / "genotypes" / "transmissions"
     assert database.variants_path == sake_path / "{target}" / "variants.parquet"
 
 
@@ -29,13 +35,22 @@ def test_set_value() -> None:
 
     database = Sake(
         sake_path,
+        threads=23,
         aggregations_path=sake_path / "other",
         annotations_path=sake_path / "other",
+        partitions_path=sake_path / "other",
         samples_path=sake_path / "other",
+        transmissions_path=sake_path / "other",
         variants_path=sake_path / "other",
     )
 
+    assert database.sake_path == sake_path
+
+    assert os.environ["POLARS_MAX_THREADS"] == str(23)
+
     assert database.aggregations_path == sake_path / "other"
     assert database.annotations_path == sake_path / "other"
+    assert database.partitions_path == sake_path / "other"
     assert database.samples_path == sake_path / "other"
+    assert database.transmissions_path == sake_path / "other"
     assert database.variants_path == sake_path / "other"
